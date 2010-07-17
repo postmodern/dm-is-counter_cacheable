7cbc316bc83415dffed82728eade531cadc97df9
Postmodern <postmodern.mod3@gmail.com>
2010-07-17 14:44:33 -0700

    Call has_property? on the PropertySet.

--- spec/integration/counter_cacheable_spec.rb
+++ spec/integration/counter_cacheable_spec.rb
@@ -14,11 +14,11 @@ describe DataMapper::Is::CounterCacheable do
   end
 
   it "should define the default counter cache property" do
-    Post.properties.should have_key('comments_counter')
+    Post.properties.should have_property('comments_counter')
   end
 
   it "should allow defining custom named counter cache properties" do
-    User.properties.should have_key('post_comments_counter')
+    User.properties.should have_property('post_comments_counter')
   end
 
   it "should have a counter cache of 0 by default" do
